31547f5c66ed9283850bc2e48be6a7471f0b7305
Edward1yadidLevi <142442624+Edward1yadidLevi@users.noreply.github.com>
2023-09-09 22:20:41 -0700

    .

--- script.js
+++ script.js
@@ -1,114 +1,77 @@
-//inputMajor
+// Elements
 const inputCalculator = document.getElementById("input-calculator");
-
-//borad of the calculator
 const calculatorBord = document.getElementById("calculator");
-const button = document.getElementById("result");
+const resultButton = document.getElementById("result");
 const bC = document.getElementById("bC");
 const header = document.getElementById("header");
 
-//result input
-const inpuText = document.getElementById("inpuText");
-let valueOfinput = 0;
-const arrayData = [];
-let answer = 0;
+// Variables
+let number = "";
+let operator = "";
+let calculationArray = [];
 
-calculatorBord.childNodes.forEach((value) => {
-  value.addEventListener("click", clickCalcutalate);
-  function clickCalcutalate() {
-    valueOfinput = value.innerText;
-    inputCalculator.value = valueOfinput;
-    arrayData.push(valueOfinput);
-    console.log(arrayData);
-    value.style.backgroundColor = `rgb(${Math.random() * 255}, ${
-      Math.random() * 255
-    }, ${Math.random() * 255})`;
+// Event listeners for number buttons
+const numbersButtons = document.querySelectorAll(".button-calculator");
+numbersButtons.forEach((button) => {
+  button.addEventListener("click", () => insertNumber(button.innerText));
+});
 
-    header.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
-      Math.random() * 255
-    })`;
-    header.style.backgroundColor = `rgb(${Math.random() * 255}, ${
-      Math.random() * 255
-    }, ${Math.random() * 255})`;
+// Event listeners for operator buttons
+const operatorsButtons = document.querySelectorAll(".button-operator");
+operatorsButtons.forEach((button) => {
+  button.addEventListener("click", () => insertOperator(button.innerText));
+});
 
-    switch (arrayData.some((value) => value)) {
-      case arrayData.some((value) => value == "+"):
-        {
-          if (arrayData.length == 3) {
-            let outPutPluse = 0;
-            outPutPluse = Number(arrayData[0]) + Number(arrayData[2]);
-            answer += outPutPluse;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          } else if (arrayData.length == 2 && arrayData.indexOf("+") == 0) {
-            outPutPluse = Number(arrayData[1]);
-            answer += outPutPluse;
-            inpuText.innerHTML = `${answer}`;
+// Event listener for result button
+resultButton.addEventListener("click", calculate);
 
-            arrayData.splice(0, arrayData.length);
-          }
-        }
-        break;
+// Event listener to clear input and arrays
+bC.addEventListener("click", clearCalculator);
 
-      case arrayData.some((value) => value == "-"):
-        {
-          if (arrayData.length == 3) {
-            outPutSubtraction = Number(arrayData[0]) - Number(arrayData[2]);
-            answer += outPutSubtraction;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          } else if (arrayData.length == 2 && arrayData.indexOf("-") == 0) {
-            outPutSubtraction = Number(arrayData[1]);
-            answer -= outPutSubtraction;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          }
-        }
+// Function to insert a number
+function insertNumber(value) {
+  number += value;
+  inputCalculator.innerText += value;
+}
 
-        break;
-
-      case arrayData.some((value) => value == "/"):
-        {
-          if (arrayData.length == 3) {
-            outPutDivision = Number(arrayData[0]) / Number(arrayData[2]);
-            answer += outPutDivision;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          } else if (arrayData.length == 2 && arrayData.indexOf("/") == 0) {
-            outPutDivision = Number(arrayData[1]);
-            answer /= outPutDivision;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          }
-        }
-        break;
-      case arrayData.some((value) => value == "*"):
-        {
-          if (arrayData.length == 3) {
-            Multiplication = Number(arrayData[0]) * Number(arrayData[2]);
-            answer += Multiplication;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          } else if (arrayData.length == 2 && arrayData.indexOf("*") == 0) {
-            Multiplication = Number(arrayData[1]);
-            answer *= Multiplication;
-            inpuText.innerHTML = `${answer}`;
-            arrayData.splice(0, arrayData.length);
-          }
-        }
-        break;
-    }
+// Function to insert an operator
+function insertOperator(value) {
+  if (number !== "") {
+    calculationArray.push(number);
+    calculationArray.push(value);
+    console.log(calculationArray);
+    inputCalculator.innerText += value;
+    number = "";
   }
-});
+}
 
-// show rsulet
-button.addEventListener("click", function () {
-  inputCalculator.value = answer;
-});
+// Function to perform the calculation
+function calculate() {
+  try {
+    if (number !== "") {
+      calculationArray.push(number);
+      console.log(calculationArray);
+      const result = eval(calculationArray.join(" "));
+      inputCalculator.innerText = result;
+    }
+  } catch {
+    alert("please reset the calculator");
+  }
+}
 
-//clear input and array
-bC.addEventListener("click", function () {
-  inputCalculator.value = 0;
-  arrayData.splice(0, arrayData.length);
-  inpuText.innerHTML = 0;
-});
+// Function to clear input and arrays
+function clearCalculator() {
+  inputCalculator.innerText = "";
+  number = "";
+  operator = "";
+  calculationArray = [];
+  document.querySelectorAll("button").forEach((button) => {
+    button.style.backgroundColor = "none";
+  });
+}
+
+function chacngeColor(event) {
+  event.style.backgroundColor = `rgb(${Math.random() * 255}, ${
+    Math.random() * 255
+  }, ${Math.random() * 255})`;
+}
